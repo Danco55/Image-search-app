36ed28102457e3ff6d0c137ce834c6c5944055d8
Daniel <136647628+Danco55@users.noreply.github.com>
2023-12-17 17:48:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function searchImages() {
   const response = await fetch(url);
   const data = await response.json();
   if (page === 1) {
-    searchResultsEl.innerHTML = "";
+    searchResultsEl.innerHTML = ""; 
   }
 
   const results = data.results;
